54cb43b01973c4bbd50b82fd3e3822ed80de5ea0
Shanmugarajan <shanmugarajan.33@gmail.com>
2023-07-21 11:13:58 +0530

    webpack configured instead of vite

---
+++ webpack.mix.js
@@ -0,0 +1,30 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.webpackConfig(webpack => {
+    return {
+        plugins: [
+            new webpack.ProvidePlugin({
+                Vue: 'vue',
+                $: 'jquery',
+                jQuery: 'jquery',
+            }),
+        ]
+    };
+});
+
+mix.setResourceRoot('../');
+
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .vue();
