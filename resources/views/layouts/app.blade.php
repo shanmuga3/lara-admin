e1c164ee37c9558e4a7ee6b61471872322060b3d
Shanmugarajan <shanmugarajan.33@gmail.com>
2023-07-21 15:26:53 +0530

    logout functions added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -163,7 +163,7 @@
                         <li class="nav-item dropdown user-menu">
                             <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
                                 <img src="{{ asset('images/user2-160x160.jpg') }}" class="user-image rounded-circle shadow" alt="User Image">
-                                <span class="d-none d-md-inline">Alexander Pierce</span>
+                                <span class="d-none d-md-inline">{{ Auth::user()->name }}</span>
                             </a>
                             <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end">
                                 <!--begin::User Image-->
@@ -171,32 +171,15 @@
                                     <img src="{{ asset('images/user2-160x160.jpg') }}" class="rounded-circle shadow" alt="User Image">
 
                                     <p>
-                                        Alexander Pierce - Web Developer
-                                        <small>Member since Nov. 2023</small>
+                                        {{ Auth::user()->name }}
+                                        <small> {{ Auth::user()->email }} </small>
                                     </p>
                                 </li>
                                 <!--end::User Image-->
-                                <!--begin::Menu Body-->
-                                <li class="user-body">
-                                    <!--begin::Row-->
-                                    <div class="row">
-                                        <div class="col-4 text-center">
-                                            <a href="#">Followers</a>
-                                        </div>
-                                        <div class="col-4 text-center">
-                                            <a href="#">Sales</a>
-                                        </div>
-                                        <div class="col-4 text-center">
-                                            <a href="#">Friends</a>
-                                        </div>
-                                    </div>
-                                    <!--end::Row-->
-                                </li>
-                                <!--end::Menu Body-->
                                 <!--begin::Menu Footer-->
                                 <li class="user-footer">
                                     <a href="#" class="btn btn-default btn-flat">Profile</a>
-                                    <a href="#" class="btn btn-default btn-flat float-end">Sign out</a>
+                                    <a href="{{ route('logout') }}" class="btn btn-default btn-flat float-end">Sign out</a>
                                 </li>
                                 <!--end::Menu Footer-->
                             </ul>
